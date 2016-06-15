d014475484d737f7432e24da0b0714d15d127f38
Johannes Steu <hello@johannessteu.de>
2016-06-15 22:15:28 +0200

    TASK: Add detail view for nodes

--- Classes/Neos/ACLInspector/Controller/Module/ACLInspectorController.php
+++ Classes/Neos/ACLInspector/Controller/Module/ACLInspectorController.php
@@ -7,18 +7,14 @@ namespace Neos\ACLInspector\Controller\Module;
 
 use Neos\ACLInspector\Dto\ACLCheckerDto;
 use Neos\ACLInspector\Service\ACLCheckerService;
+use TYPO3\Eel\FlowQuery\FlowQuery;
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Flow\Security\Policy\PolicyService;
 use TYPO3\Neos\Controller\Module\AbstractModuleController;
+use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
 
 class ACLInspectorController extends AbstractModuleController
 {
-
-    /**
-     * @Flow\Inject
-     * @var ACLCheckerService
-     */
-    protected $aclCheckService;
 
     /**
      * @Flow\Inject
@@ -27,11 +23,17 @@ class ACLInspectorController extends AbstractModuleController
     protected $policyService;
 
     /**
+     * @Flow\Inject
+     * @var ACLCheckerService
+     */
+    protected $aclCheckService;
+
+    /**
      * @return void
      */
     public function indexAction(ACLCheckerDto $dto = null)
     {
-        if($dto === null) {
+        if ($dto === null) {
             $dto = new ACLCheckerDto();
         }
 
@@ -46,4 +48,41 @@ class ACLInspectorController extends AbstractModuleController
         );
     }
 
+    /**
+     * @param NodeInterface $node
+     */
+    public function showAction(NodeInterface $node)
+    {
+        $roles = $this->policyService->getRoles(true);
+
+        $this->view->assignMultiple([
+                'acl' => $this->aclCheckService->checkNodeForRoles($node, $roles),
+                'targets' => $this->aclCheckService->checkPrivilegeTargetsForNodeAndRoles($node, $roles),
+                'node' => $node,
+                'breadcrumbNodes' => $this->breadcrumbNodesForNode($node),
+                'childNodes' => $this->aclCheckService->getContentNodes($node, $roles, 999)
+        ]);
+    }
+
+    /**
+     * @param NodeInterface $node
+     * @return array
+     */
+    protected function breadcrumbNodesForNode(NodeInterface $node)
+    {
+        $documentNodes = [];
+        $flowQuery = new FlowQuery(array($node));
+        $nodes = array_reverse($flowQuery->parents('[instanceof TYPO3.Neos:Document]')->get());
+
+        /** @var NodeInterface $node */
+        foreach ($nodes as $documentNode) {
+            $documentNodes[] = $documentNode;
+        }
+
+        if ($node->getNodeType()->isOfType('TYPO3.Neos:Document')) {
+            $documentNodes[] = $node;
+        }
+
+        return $documentNodes;
+    }
 }
--- Classes/Neos/ACLInspector/Service/ACLCheckerService.php
+++ Classes/Neos/ACLInspector/Service/ACLCheckerService.php
@@ -6,6 +6,7 @@ namespace Neos\ACLInspector\Service;
  */
 use Neos\ACLInspector\Dto\ACLCheckerDto;
 use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Security\Authorization\Privilege\PrivilegeInterface;
 use TYPO3\Flow\Security\Authorization\PrivilegeManagerInterface;
 use TYPO3\Flow\Security\Exception\NoSuchRoleException;
 use TYPO3\Flow\Security\Policy\PolicyService;
@@ -15,10 +16,12 @@ use TYPO3\Neos\Security\Authorization\Privilege\NodeTreePrivilege;
 use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
 use TYPO3\TYPO3CR\Domain\Model\NodeLabelGeneratorInterface;
 use TYPO3\TYPO3CR\Domain\Service\ContextFactoryInterface;
+use TYPO3\TYPO3CR\Security\Authorization\Privilege\Node\AbstractNodePrivilege;
 use TYPO3\TYPO3CR\Security\Authorization\Privilege\Node\CreateNodePrivilege;
 use TYPO3\TYPO3CR\Security\Authorization\Privilege\Node\CreateNodePrivilegeSubject;
 use TYPO3\TYPO3CR\Security\Authorization\Privilege\Node\EditNodePrivilege;
 use TYPO3\TYPO3CR\Security\Authorization\Privilege\Node\NodePrivilegeSubject;
+use TYPO3\TYPO3CR\Security\Authorization\Privilege\Node\ReadNodePrivilege;
 use TYPO3\TYPO3CR\Security\Authorization\Privilege\Node\RemoveNodePrivilege;
 
 class ACLCheckerService
@@ -73,7 +76,7 @@ class ACLCheckerService
         foreach ($roles as $role) {
             /** @var Role $role */
             $checkedNodes[$role->getIdentifier()] = [
-                'editNode' => $this->privilegeManager->isGrantedForRoles([$role], EditNodePrivilege::class, new NodePrivilegeSubject($node), $debug),
+                'editNode' => $this->privilegeManager->isGrantedForRoles([$role], EditNodePrivilege::class, new NodePrivilegeSubject($node)),
                 'removeNode' => $this->privilegeManager->isGrantedForRoles([$role], RemoveNodePrivilege::class, new NodePrivilegeSubject($node)),
                 'createNodeOfType' => $this->privilegeManager->isGrantedForRoles([$role], CreateNodePrivilege::class, new CreateNodePrivilegeSubject($node)),
                 'showInTree' => $this->privilegeManager->isGrantedForRoles([$role], NodeTreePrivilege::class, new NodePrivilegeSubject($node))
@@ -83,12 +86,62 @@ class ACLCheckerService
     }
 
     /**
+     * @param NodeInterface $node
+     * @param array $roles
+     * @return array
+     */
+    public function checkPrivilegeTargetsForNodeAndRoles(NodeInterface $node, array $roles)
+    {
+        $result = [
+            'denied' => [],
+            'abstained' => [],
+            'granted' => []
+        ];
+
+        foreach ($roles as $role) {
+            /** @var Role $role */
+            foreach ($role->getPrivileges() as $privilege) {
+                /** @var PrivilegeInterface $privilege */
+
+                // todo the check for ReadNodePrivilege can be removed if its method `matchesSubject()` is implemented
+                if (!$privilege instanceof ReadNodePrivilege && !$privilege instanceof AbstractNodePrivilege) {
+                    continue;
+                }
+
+                if ($privilege instanceof CreateNodePrivilege) {
+                    $nodeSubject = new CreateNodePrivilegeSubject($node, $node->getNodeType());
+                } else {
+                    $nodeSubject = new NodePrivilegeSubject($node);
+                }
+
+                if (!$privilege->matchesSubject($nodeSubject)) {
+                    continue;
+                }
+
+                $resultArray = ['privilege' => $privilege->getPrivilegeTargetIdentifier(), 'role' => $role->getIdentifier()];
+
+                if ($privilege->isDenied()) {
+                    $result['denied'][] = $resultArray;
+                }
+                if ($privilege->isAbstained()) {
+                    $result['abstained'][] = $resultArray;
+                }
+                if ($privilege->isGranted()) {
+                    $result['granted'][] = $resultArray;
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    /**
      * @param ACLCheckerDto $dto
      * @return array
      */
     protected function getNodes(ACLCheckerDto $dto)
     {
-        $context = $this->contextFactory->create(array('workspaceName' => 'live'));
+        $context =  $this->createContext();
 
         $site = $this->siteRepository->findFirstOnline();
         $startNode = $context->getNode('/sites/' . $site->getNodeName());
@@ -97,6 +150,26 @@ class ACLCheckerService
 
         $nodes = [];
         $this->getChildNodeData($nodes, $startNode, $roles, $dto->getNodeTreeLoadingDepth());
+
+        return $nodes;
+    }
+
+    public function getContentNodes(NodeInterface $node, $roles, $nodeTreeLoadingDepth = 4)
+    {
+        $context = $this->createContext();
+        $nodes = [];
+
+        if ($node->getNodeType()->isOfType('TYPO3.Neos.Document')) {
+            foreach ($node->getChildNodes('TYPO3.Neos:ContentCollection') as $childNode) {
+                $properties = $this->getACLPropertiesForNode($childNode);
+                $properties['acl'] = $this->checkNodeForRoles($childNode, $roles);
+                $nodes[] = $properties;
+                $this->getChildNodeData($nodes, $childNode, $roles, $nodeTreeLoadingDepth, 1, null);
+            }
+        } else {
+            $this->getChildNodeData($nodes, $node, $roles, $nodeTreeLoadingDepth, 1, null);
+        }
+
 
         return $nodes;
     }
@@ -131,14 +204,8 @@ class ACLCheckerService
             /** @var NodeInterface $childNode */
             $expand = ($depth === 0 || $recursionPointer < $depth);
 
-            $properties = [
-                'nodeIdentifier' => $childNode->getIdentifier(),
-                'nodePath' => $childNode->getPath(),
-                'nodeLabel' => $childNode->getLabel(),
-                'nodeType' => $childNode->getNodeType()->getName(),
-                'nodeLevel' => $childNode->getDepth(),
-                'acl' => $this->checkNodeForRoles($childNode, $roles)
-            ];
+            $properties = $this->getACLPropertiesForNode($childNode);
+            $properties['acl'] = $this->checkNodeForRoles($node, $roles);
 
             if($expand && $childNode->hasChildNodes($nodeTypeFilter)) {
                 $properties['childNodes'] = [];
@@ -147,5 +214,26 @@ class ACLCheckerService
 
             array_push($nodes, $properties);
         }
+    }
+
+    protected function getACLPropertiesForNode(NodeInterface $node)
+    {
+        $properties = [
+            'nodeIdentifier' => $node->getIdentifier(),
+            'nodePath' => $node->getPath(),
+            'nodeLabel' => $node->getLabel(),
+            'nodeType' => $node->getNodeType()->getName(),
+            'nodeLevel' => $node->getDepth(),
+        ];
+        return $properties;
+    }
+
+    /**
+     * @return \TYPO3\TYPO3CR\Domain\Service\Context
+     */
+    protected function createContext()
+    {
+        $context = $this->contextFactory->create(array('workspaceName' => 'live'));
+        return $context;
     }
 }
